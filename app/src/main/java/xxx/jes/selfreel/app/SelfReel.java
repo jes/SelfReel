d87cebb7cbd36ef9a5679267fe462b7b28cbb563
James Stanley <james@incoherency.co.uk>
2014-04-23 00:48:18 +0100

    Take picture at startup

--- app/src/main/java/xxx/jes/selfreel/app/SelfReel.java
+++ app/src/main/java/xxx/jes/selfreel/app/SelfReel.java
@@ -2,10 +2,13 @@ package xxx.jes.selfreel.app;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
@@ -16,7 +19,12 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SelfReel extends Activity {
 
@@ -124,6 +132,7 @@ public class SelfReel extends Activity {
             try {
                 Camera.Parameters params = mCamera.getParameters();
                 params.setRecordingHint(true);
+                params.set("rotation", 270);
                 mCamera.setParameters(params);
                 mCamera.setPreviewDisplay(mHolder);
                 mCamera.startPreview();
@@ -131,6 +140,79 @@ public class SelfReel extends Activity {
             } catch (Exception e){
                 Log.d("TAG", "Error starting camera preview: " + e.getMessage());
             }
+
+
+
+            Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+                    if (pictureFile == null){
+                        Log.d("TAG", "Error creating media file, check storage permissions.");
+                        return;
+                    }
+
+                    try {
+                        FileOutputStream fos = new FileOutputStream(pictureFile);
+                        fos.write(data);
+                        fos.close();
+                        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                        intent.setData(Uri.fromFile(pictureFile));
+                        sendBroadcast(intent);
+                        mCamera.startPreview();
+                    } catch (FileNotFoundException e) {
+                        Log.d("TAG", "File not found: " + e.getMessage());
+                    } catch (IOException e) {
+                        Log.d("TAG", "Error accessing file: " + e.getMessage());
+                    }
+                }
+            };
+
+            mCamera.takePicture(null, null, mPicture);
+        }
+
+        public static final int MEDIA_TYPE_IMAGE = 1;
+        public static final int MEDIA_TYPE_VIDEO = 2;
+
+        /** Create a file Uri for saving an image or video */
+        private Uri getOutputMediaFileUri(int type){
+            return Uri.fromFile(getOutputMediaFile(type));
+        }
+
+        /** Create a File for saving an image or video */
+        private File getOutputMediaFile(int type){
+            // To be safe, you should check that the SDCard is mounted
+            // using Environment.getExternalStorageState() before doing this.
+
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES), "SelfReel");
+            Log.d("TAG", "ExternalStoragePublicDirectory = " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+            // This location works best if you want the created images to be shared
+            // between applications and persist after your app has been uninstalled.
+
+            // Create the storage directory if it does not exist
+            if (! mediaStorageDir.exists()){
+                if (! mediaStorageDir.mkdirs()){
+                    Log.d("SelfReel", "failed to create directory");
+                    return null;
+                }
+            }
+
+            // Create a media file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            File mediaFile;
+            if (type == MEDIA_TYPE_IMAGE){
+                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                        "IMG_"+ timeStamp + ".jpg");
+            } else if(type == MEDIA_TYPE_VIDEO) {
+                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                        "VID_"+ timeStamp + ".mp4");
+            } else {
+                return null;
+            }
+
+            return mediaFile;
         }
     }
 }
